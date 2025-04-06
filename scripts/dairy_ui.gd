1900dc4ac308c9e6bba9f7d9b39d86837aac27d5
Gert1337 <Katharinaappel@gmail.com>
2025-04-06 14:31:48 +0200

    make diary navigation circular

--- scripts/dairy_ui.gd
+++ scripts/dairy_ui.gd
@@ -33,17 +33,13 @@ func show_page(index:int):
 func _input(event: InputEvent) -> void:
 	if not visible:
 		return
+	var page_count =GameManager.diary_entries_storage.size()
 	if event.is_action_pressed("diary_page_forward"):
-		if current_page < GameManager.diary_entries_storage.size() - 1:
-			current_page +=1
+			current_page = (current_page + 1) % page_count
 			show_page(current_page)
 	if event.is_action_pressed("diary_page_back"):
-		if current_page > 0:
-			current_page -=1
+			current_page =(current_page - 1 + page_count) % page_count
 			show_page(current_page)	
-	#if event.is_action_pressed("Open Diary"):
-		#print("cancel")
-		#hide()
 	
 func hide_diary():
 	hide()
